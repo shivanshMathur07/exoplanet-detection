3bea25819f5ec0b4af7aaa4b0e9e8fbe168d2225
shivanshMathur07 <shivanshmathurr@gmail.com>
2022-05-01 23:43:24 +0530

    improved folder templates

--- web/app.py
+++ web/app.py
@@ -2,13 +2,13 @@ from flask import Flask,render_template
 import pandas as pd
 from prediction_helper import my_prediction
 
-# DUMMY_DATA = pd.DataFrame({"dummy1":[0],"dummy2":[0]})
-
 app = Flask(__name__)
+
+# 127.0.0.1:5000/predict
 
 @app.route('/')
 def index():
-    return render_template("navbar.html")
+    return render_template("homepage.html")
 
 @app.route("/predict")
 def predict():
