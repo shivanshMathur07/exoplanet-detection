a252ebc1aa0b3bd4b2b444f037751ea7de2e0568
shivanshMathur07 <shivanshmathurr@gmail.com>
2022-05-04 23:13:46 +0530

    Backend Work Done..Fixed Errors

--- web/prediction_helper/my_prediction.py
+++ web/prediction_helper/my_prediction.py
@@ -11,7 +11,7 @@ trained_model = pickle.load(pickle_in)
 
 def get_prediction(data):
     # print(data.shape)
-    data = data.drop(['kepid','kepoi_name','kepler_name','koi_pdisposition','koi_score','koi_teq_err1','koi_teq_err2'],axis=1)
+    data = data.drop(['kepid','kepoi_name','kepler_name','koi_pdisposition','koi_disposition','koi_score','koi_teq_err1','koi_teq_err2'],axis=1)
     data['koi_tce_delivname'] = data['koi_tce_delivname'].fillna(data['koi_tce_delivname'].mode()[0])
     #for filling numerical empty values
     for col in data.columns[data.isna().sum() > 0] :
@@ -21,8 +21,15 @@ def get_prediction(data):
     data = pd.concat([data, delivname_dummies], axis=1)
     data = data.drop('koi_tce_delivname', axis=1)
     #since we have to add two new features in data we are using dummies
-    data['delivname_q1_q16_tce'] = 0
-    data['delivname_q1_q17_dr24_tce'] = 0
+    if('delivname_q1_q16_tce' in data.columns):
+        data['delivname_q1_q17_dr25_tce'] = 0
+        data['delivname_q1_q17_dr24_tce'] = 0
+    elif('delivname_q1_q17_dr24_tce' in data.columns):
+        data['delivname_q1_q16_tce'] = 0
+        data['delivname_q1_q17_dr25_tce'] = 0
+    elif('delivname_q1_q17_dr25_tce' in data.columns):
+        data['delivname_q1_q16_tce'] = 0
+        data['delivname_q1_q17_dr24_tce'] = 0
     results = trained_model.predict(data)
     if(results[0] == 1):
         return("Exoplanet Candidate")
